fb67dd85c0d1c5e37de8d4295d121eeb3af78005
sarahpampalone <120332370+sarahpampalone@users.noreply.github.com>
2022-12-29 12:56:05 -0500

    Update palindromenumber.py

--- palindromenumber.py
+++ palindromenumber.py
@@ -1,4 +1,4 @@
-/* Given an integer x, return true if x is a palindrome, and false otherwise */
+# Given an integer x, return true if x is a palindrome, and false otherwise
 
 class Solution(object) :
   def isPalindrome(self, x) :
