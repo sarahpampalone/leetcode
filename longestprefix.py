2f795114f246c0cc490a072f8a17e0f7881471b4
sarahpampalone <120332370+sarahpampalone@users.noreply.github.com>
2022-12-29 12:55:47 -0500

    Update longestprefix.py

--- longestprefix.py
+++ longestprefix.py
@@ -1,4 +1,4 @@
-/* Write a function to find the longest common prefix string amongst an array of strings. If there is no common prefix, return an empty string “” */
+#Write a function to find the longest common prefix string amongst an array of strings. If there is no common prefix, return an empty string “”
 
 class Solution(object):
     def mergeTwoLists(self, list1, list2):
